85b24cb3b338c0ebc7097af537d1bebba19e123d
thenileshmishra <thenileshmishra@gmail.com>
2026-02-11 20:14:52 +0530

    Update SSL setup script with user email

--- setup-ssl.sh
+++ setup-ssl.sh
@@ -71,7 +71,7 @@ echo ""
 sudo certbot certonly \
   --webroot \
   --webroot-path=/var/www/certbot \
-  --email your-email@example.com \
+  --email thenileshmishra@gmail.com \
   --agree-tos \
   --no-eff-email \
   -d agent.nileshmishra.info
